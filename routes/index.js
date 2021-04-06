4ed4f955feca76c4c8ee2ae95ce90ed69bc13cf2
fofona <leilafofana.makoko@ynov.com>
2021-04-06 22:04:27 +0100

    premiercommit

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.render('index');
+}); /* get home page */
+router.post('/home', function(req, res, next) {
+    res.render('home');
+});
+router.get('/register', function(req, res, next) {
+    res.render('register');
+});
+module.exports = router;
